e39ddbf0aaa7b8f299e36efd7f13b16446b10f50
johnshelb <jsnd11@gmail.com>
2017-03-20 16:58:05 -0400

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,18 +1,26 @@
-const expect = require('expect')
-
-const fs = require('fs')
-const jsdom = require('mocha-jsdom')
-const path = require('path')
-
-
-describe('index', () => {
-  
-  jsdom({
-    src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
-  })
-  
-
-  it('runs', () => {
-    expect(true).to.be.true
-  })
-})
+// const expect = require('expect')
+//
+// const fs = require('fs')
+// const jsdom = require('mocha-jsdom')
+// const path = require('path')
+//
+//
+// describe('index', () => {
+//
+//   jsdom({
+//     src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+//   })
+//
+//
+//   it('runs', () => {
+//     expect(true).to.be.true
+//   })
+// })
+var assert = require('assert');
+describe('Array', function() {
+describe('#indexOf()', function() {
+  it('should return -1 when the value is not present', function() {
+    assert.equal(-1, [1,2,3].indexOf(4));
+  });
+});
+});
